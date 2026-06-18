7229a804ddca5c9cde8f367b6e8fbc45a6500919
qwx <qwxiae@gmail.com>
2026-06-19 02:36:05 +0300

    fix: use naming convention in Base to avoid uq-naming problems in Alembic

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,18 @@
-from sqlalchemy import create_engine 
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 from app.core.config import settings
 from typing import Generator, Any
 
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=NAMING_CONVENTION)
 
 
 engine = create_engine(
